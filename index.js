371a48aa532e0532573537f1e684d86b5f1b29e0
TohilGuerra <tohil6@gmail.com>
2023-03-19 12:16:41 -0500

    adds conditions

--- index.js
+++ index.js
@@ -1,11 +1,39 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(feet){
   // Write your code here!
+  if (feet <= 400) {
+    return "This one is on me!"
+    //console.log();
+  }
+  else if (feet > 400 && feet <= 2000) {
+    return "That will be twenty bucks."
+  }
+  else if (feet > 2000 && feet < 2500) {
+    return "I will gladly take your thirty bucks."
+  } else {
+    return "No can do."
+  }
 }
+console.log(scuberGreetingForFeet(2499));
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  let cityResponse = city === "NYC" ? "Ok, sounds good." : "No go."  
+  return cityResponse;  
 }
+console.log(ternaryCheckCity("NYC"));
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  switch (tip) {
+    case 'generous':
+      return 'Thank you so much.';
+      //break;
+    case 'not as generous':
+      return 'Thank you.';
+      //break;
+    default:
+      return 'Bye.';
+      //break;
+  }
 }
+console.log(switchOnCharmFromTip('not as generous'));
